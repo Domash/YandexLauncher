13d10e8bbb2911d3d4097bfd2c657f810a59bad8
Domash <35339409+Domash@users.noreply.github.com>
2019-02-03 18:30:11 +0300

    feature: add icon and personal page

--- app/src/main/java/com/domash/yandexlauncher/RoundedImageView.kt
+++ app/src/main/java/com/domash/yandexlauncher/RoundedImageView.kt
@@ -10,10 +10,10 @@ import android.graphics.PorterDuff.Mode
 import android.graphics.PorterDuffXfermode
 import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
+import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
-import android.widget.ImageView
 
-class RoundedImageView(ctx: Context, attrs: AttributeSet) : ImageView(ctx, attrs) {
+class RoundedImageView(ctx: Context, attrs: AttributeSet) : AppCompatImageView(ctx, attrs) {
 
     override fun onDraw(canvas: Canvas) {
 
