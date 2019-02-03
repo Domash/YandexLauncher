d8e4d93a68c2f1453ce879ffb6ce03d13fa8d837
Domash <35339409+Domash@users.noreply.github.com>
2019-02-04 01:29:16 +0300

    feature: change workflows options

--- app/src/main/java/com/domash/yandexlauncher/MainActivity.kt
+++ app/src/main/java/com/domash/yandexlauncher/MainActivity.kt
@@ -3,10 +3,18 @@ package com.domash.yandexlauncher
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+//Fabric
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Fabric
+        Fabric.with(this, Crashlytics())
+
         setContentView(R.layout.activity_main)
     }
 }
