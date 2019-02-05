dcca15ac58a7324d77046d6f54a3d9ddb07e00de
Domash <35339409+Domash@users.noreply.github.com>
2019-02-05 20:51:07 +0300

    feature: Add AppCenter

--- app/src/main/java/com/domash/yandexlauncher/MainActivity.kt
+++ app/src/main/java/com/domash/yandexlauncher/MainActivity.kt
@@ -8,9 +8,9 @@ import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 
 //AppCentre
-//import com.microsoft.appcenter.AppCenter
-//import com.microsoft.appcenter.analytics.Analytics
-//import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,10 +21,10 @@ class MainActivity : AppCompatActivity() {
         Fabric.with(this, Crashlytics())
 
         //AppCentre
-        //AppCenter.start(
-        //    application, BuildConfig.APPCENTER_KEY,
-        //    Analytics::class.java, Crashes::class.java
-        //)
+        AppCenter.start(
+            application, BuildConfig.APPCENTER_KEY,
+            Analytics::class.java, Crashes::class.java
+        )
 
         setContentView(R.layout.activity_main)
     }
